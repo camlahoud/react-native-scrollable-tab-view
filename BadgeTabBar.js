6d48acee37c6f3e978905bbfa8a5d3bb9f7386e8
Camille Lahoud <camlahoud@gmail.com>
2016-11-28 17:10:28 +0200

    Added a new type of tab bar that supports badges

---
+++ BadgeTabBar.js
@@ -0,0 +1,119 @@
+const React = require('react');
+const ReactNative = require('react-native');
+const {
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  Platform
+} = ReactNative;
+const Button = require('./Button');
+
+const BadgeTabBar = React.createClass({
+  propTypes: {
+    goToPage: React.PropTypes.func,
+    activeTab: React.PropTypes.number,
+    tabs: React.PropTypes.array,
+    backgroundColor: React.PropTypes.string,
+    activeTextColor: React.PropTypes.string,
+    inactiveTextColor: React.PropTypes.string,
+    textStyle: Text.propTypes.style,
+    tabStyle: View.propTypes.style,
+    renderTab: React.PropTypes.func,
+    underlineStyle: View.propTypes.style,
+  },
+
+  getDefaultProps() {
+    return {
+      activeTextColor: 'navy',
+      inactiveTextColor: 'black',
+      backgroundColor: null,
+      badges: []
+    };
+  },
+
+  renderTabOption(name, page) {
+  },
+
+  renderTab(name, page, isTabActive, onPressHandler) {
+    const { activeTextColor, inactiveTextColor, textStyle, } = this.props;
+    const textColor = isTabActive ? activeTextColor : inactiveTextColor;
+    const fontWeight = isTabActive ? 'bold' : 'normal';
+    const badge = this.props.badges[page] || null;
+    return <Button
+      style={{flex: 1, }}
+      key={name}
+      accessible={true}
+      accessibilityLabel={name}
+      accessibilityTraits='button'
+      onPress={() => onPressHandler(page)}
+    >
+      <View style={[styles.tab, this.props.tabStyle, ]}>
+        <Text style={[{color: textColor, fontWeight, }, textStyle, ]}>
+          {name}
+        </Text>
+        {badge?<View style={styles.badge}><Text style={styles.badgeText}>{badge}</Text></View>:null}
+      </View>
+    </Button>;
+  },
+
+  render() {
+    const containerWidth = this.props.containerWidth;
+    const numberOfTabs = this.props.tabs.length;
+    const tabUnderlineStyle = {
+      position: 'absolute',
+      width: containerWidth / numberOfTabs,
+      height: 4,
+      backgroundColor: 'navy',
+      bottom: 0,
+    };
+
+    const left = this.props.scrollValue.interpolate({
+      inputRange: [0, 1, ], outputRange: [0,  containerWidth / numberOfTabs, ],
+    });
+    return (
+      <View style={[styles.tabs, {backgroundColor: this.props.backgroundColor, }, this.props.style, ]}>
+        {this.props.tabs.map((name, page) => {
+          const isTabActive = this.props.activeTab === page;
+          const renderTab = this.props.renderTab || this.renderTab;
+          return renderTab(name, page, isTabActive, this.props.goToPage);
+        })}
+        <Animated.View style={[tabUnderlineStyle, { left, }, this.props.underlineStyle, ]} />
+      </View>
+    );
+  },
+});
+
+const styles = StyleSheet.create({
+  tab: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: 10,
+    flexDirection: 'row'
+  },
+  tabs: {
+    height: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    borderWidth: 1,
+    borderTopWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderColor: '#ccc',
+  },
+  badge: {
+      backgroundColor:"#f7324e",
+        padding: (Platform.OS === 'ios') ? 3 : 0,
+        paddingHorizontal: 5,
+        // alignSelf: 'flex-start',
+        borderRadius: 10,
+        height: 20,
+        marginLeft: 5
+  },
+  badgeText: {
+      color: "#FFF"
+  }
+});
+
+module.exports = BadgeTabBar;
